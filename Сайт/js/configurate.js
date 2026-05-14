e6b9e462bd14de56956746349ca76fd5f31f342d
Дрожжин Владимир Геннадьевич <v.drojjin@mkit.local>
2026-05-14 10:31:45 +0300

    vibor

--- Сайт/js/configurate.js
+++ Сайт/js/configurate.js
@@ -10,13 +10,25 @@ document.addEventListener('DOMContentLoaded', function() {
                 li.className = 'component';
                 li.textContent = component.name;
                 li.setAttribute('data-id', component.id);
-                li.addEventListener('click',() => changeCategory(component.id, component.name))
+                li.addEventListener('click',() => {
+                    changeCategory(component.id)
+                })
                 componentsList.appendChild(li);
             });
         })
+    
+    const fieldElements = document.querySelectorAll('.confidurElem');
+    fieldElements.forEach(element => {
+        element.addEventListener('click', function() {
+            const catId = this.id;
+            // const catName = this.querySelector('.choice').getAttribute('data-text');
+            changeCategory(catId);
+        });
+    });
 });
-function changeCategory(catId, catName){
-    const elementConfList = document.querySelector('.elementConfList')
+
+function changeCategory(catId){
+    const elementConfList = document.querySelector('.elementConfList-ul')
     elementConfList.innerHTML = ''
     fetch('../db/products.json')
     .then(response =>{
@@ -24,14 +36,28 @@ function changeCategory(catId, catName){
     })
     .then(products =>{
         products.forEach(item => {
-            if(catId === item.category_id){
+            if(catId == item.category_id){
                 const li = document.createElement('li');
                 li.className = 'elementConf';
                 li.innerHTML = `
-                <div className= "cart"><img src ="" alt="cart"></div>
-                <span className ="text">${item.name}</span>`
+                <img src ="" alt="cart">
+                <span class ="text">${item.name}</span>`
                 elementConfList.appendChild(li);
             }
         })
     })
+    fieldElement(catId)
 }
+
+let confFields = document.querySelectorAll('.confidurElem');
+console.log(confFields);
+
+function fieldElement(dataId){
+    confFields.forEach(element => {
+        element.classList.remove('start');
+        if (element.id == dataId){
+            element.classList.add('start');
+        }
+    });
+}
+// new Sortable()
